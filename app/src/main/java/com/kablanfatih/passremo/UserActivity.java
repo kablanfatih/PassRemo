bcc24efcac755bc6cce4e3a7a570b96efe62c8f9
Fatih Kablan <kablanfatih34@gmail.com>
2019-02-13 23:03:29 +0300

    Veritabanından kullanıcı id sine göre kayıtlar getirildi

--- app/src/main/java/com/kablanfatih/passremo/UserActivity.java
+++ app/src/main/java/com/kablanfatih/passremo/UserActivity.java
@@ -2,26 +2,38 @@ package com.kablanfatih.passremo;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class UserActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     FirebaseUser user;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference myRef;
     RecyclerView recyclerView;
     FloatingActionButton addButton;
     LinearLayout passwordInfo;
@@ -34,6 +46,7 @@ public class UserActivity extends AppCompatActivity {
         createRecyclerView();
         define();
         createPassword();
+        getDataFromFirebase();
 
         if (user == null) {
 
@@ -47,6 +60,8 @@ public class UserActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = firebaseDatabase.getReference();
         addButton = (FloatingActionButton) findViewById(R.id.addButton);
         passwordInfo = (LinearLayout) findViewById(R.id.password_info);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
@@ -136,5 +151,33 @@ public class UserActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void getDataFromFirebase() {
+
+        myRef = firebaseDatabase.getReference();
+        myRef.child("Records").child(user.getUid()).addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+
+                    HashMap<String, String> hashMap = (HashMap<String, String>) ds.getValue();
+
+                    Log.i("title", hashMap.get("password"));
+                    Log.i("allRecord", ds.getValue().toString());
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                Toast.makeText(getApplicationContext(), "Hata Oluştu Lütfen Tekrar Deneyiniz", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
 }
 
