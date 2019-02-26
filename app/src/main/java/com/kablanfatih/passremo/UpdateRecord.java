c19e5546455a5d855ba20284a2ebc19ed7e26f3a
Fatih Kablan <kablanfatih34@gmail.com>
2019-02-26 04:03:13 +0300

    parola güncelleme işlemi tamamlandı

--- app/src/main/java/com/kablanfatih/passremo/UpdateRecord.java
+++ app/src/main/java/com/kablanfatih/passremo/UpdateRecord.java
@@ -2,7 +2,6 @@ package com.kablanfatih.passremo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,9 +10,11 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.tinmegali.security.mcipher.MEncryptor;
+import com.tinmegali.security.mcipher.MEncryptorBuilder;
+import com.tinmegali.security.mcipher.exceptions.MEncryptorException;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Objects;
 
 public class UpdateRecord extends AppCompatActivity {
 
@@ -23,6 +24,7 @@ public class UpdateRecord extends AppCompatActivity {
     DatabaseReference myRef;
     Button buttonChangePassword;
     EditText editTextNewPassword;
+    MEncryptor mEncryptor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,28 +45,27 @@ public class UpdateRecord extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Bundle extras = getIntent().getExtras();
-                String recordId = extras.getString("recordId");
-                Log.i("tag",recordId);
+                String recordId = Objects.requireNonNull(extras).getString("recordId");
                 changePasswordToDatabase(recordId);
-
             }
         });
     }
 
-    public void changePasswordToDatabase(String recordId) {
+    public void changePasswordToDatabase(String recordId){
 
         mAuth = FirebaseAuth.getInstance();
         user = mAuth.getCurrentUser();
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference().child("Records").child(user.getUid());
+        myRef = database.getReference().child("Records").child(user.getUid()).child(recordId).child("password");
 
-        ListPassword listPassword = new ListPassword();
-        listPassword.setEmail(user.getEmail());
-        listPassword.setTitle("titledeneme");
-        listPassword.setName("denmeup");
-        listPassword.setPassword("passdeneme");
-        listPassword.setRecordId(recordId);
-        myRef.child(recordId).setValue(listPassword);
-
+        String ALIAS = "0https4://08digital-9transltr82firebaseio.0com/";
+        String newPassword = editTextNewPassword.getText().toString();
+        try {
+        mEncryptor = new MEncryptorBuilder(ALIAS).build();
+        String encryptedPassword = mEncryptor.encryptString(newPassword,this);
+            myRef.setValue(encryptedPassword);
+        } catch (MEncryptorException e) {
+            e.printStackTrace();
+        }
     }
 }
